355353027d99fb5a326a887ea11d498876fab416
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-04-28 23:30:27 +0800

    Updates

---
+++ precache-manifest.e9119cf7c0777c7b78c52074f617a602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65836ce94e32f3e675a3a69832d9e250",
+    "url": "/complete-react-developer-zero-to-mastery/index.html"
+  },
+  {
+    "revision": "b9b7093755d9318f4519",
+    "url": "/complete-react-developer-zero-to-mastery/static/css/main.a99b2c3c.chunk.css"
+  },
+  {
+    "revision": "874ef6dce6428a3044f2",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/2.3f91b9d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/2.3f91b9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b7093755d9318f4519",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/main.b7e38426.chunk.js"
+  },
+  {
+    "revision": "f7e910b901738af24b8a",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/runtime-main.46ed28f5.js"
+  }
+]);
